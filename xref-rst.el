c497c0d9b97692a39c984d0e6bb06dcdc4ad0082
Campbell Barton <ideasman42@gmail.com>
2022-05-23 22:34:13 +1000

    Update URL

--- xref-rst.el
+++ xref-rst.el
@@ -5,7 +5,7 @@
 
 ;; Author: Campbell Barton <ideasman42@gmail.com>
 
-;; URL: https://gitlab.com/ideasman42/emacs-xref-rst
+;; URL: https://codeberg.org/ideasman42/emacs-xref-rst
 ;; Version: 0.1
 ;; Package-Requires: ((emacs "28.1"))
 
