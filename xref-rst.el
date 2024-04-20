218f59884580131f87b43028a55d8a2e43cad675
Campbell Barton <ideasman42@gmail.com>
2024-04-20 21:08:08 +1000

    Bump emacs version 29.1

--- xref-rst.el
+++ xref-rst.el
@@ -7,7 +7,7 @@
 
 ;; URL: https://codeberg.org/ideasman42/emacs-xref-rst
 ;; Version: 0.1
-;; Package-Requires: ((emacs "28.1"))
+;; Package-Requires: ((emacs "29.1"))
 
 ;;; Commentary:
 
@@ -29,22 +29,6 @@
 (require 'vc) ; For `vc-backend' and related functions.
 (require 'xref) ; For `xref' integration.
 (require 'map) ; For `map-elt'.
-
-
-;; ---------------------------------------------------------------------------
-;; Compatibility
-
-(when (and (version< emacs-version "29.1") (not (and (fboundp 'pos-bol) (fboundp 'pos-eol))))
-  (defun pos-bol (&optional n)
-    "Return the position at the line beginning."
-    (declare (side-effect-free t))
-    (let ((inhibit-field-text-motion t))
-      (line-beginning-position n)))
-  (defun pos-eol (&optional n)
-    "Return the position at the line end."
-    (declare (side-effect-free t))
-    (let ((inhibit-field-text-motion t))
-      (line-end-position n))))
 
 
 ;; ---------------------------------------------------------------------------
